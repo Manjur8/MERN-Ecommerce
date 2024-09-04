d7589dfcbc7c2509bcd6d1f3b2b0dd9cff5e16d7
Manjur <manjursk209@gmail.com>
2024-09-04 21:10:35 +0530

    added cart ui

--- ecommerce-frontend/src/pages/cart.tsx
+++ ecommerce-frontend/src/pages/cart.tsx
@@ -1,7 +1,109 @@
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+import { Link } from "react-router-dom";
+import CartItemComponent from "../components/CartItem";
+import { CartItem } from "../types/types";
 
 const Cart = () => {
+  const cartItems: CartItem[] = [
+    {
+      productId: 'sdge444',
+      name:"Macbook",
+      price:59999,
+      stock:5,
+      quantity: 1,
+      photo:"https://m.media-amazon.com/images/I/71h-tsPzk5L._SX679_.jpg",
+    }
+  ]
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
+
+  useEffect(() => {
+    // const { token: cancelToken, cancel } = axios.CancelToken.source();
+
+    const timeOutID = setTimeout(() => {
+      // axios
+      //   .get(`${server}/api/v1/payment/discount?coupon=${couponCode}`, {
+      //     cancelToken,
+      //   })
+      //   .then((res) => {
+      //     dispatch(discountApplied(res.data.discount));
+      //     setIsValidCouponCode(true);
+      //     dispatch(calculatePrice());
+      //   })
+      //   .catch(() => {
+      //     dispatch(discountApplied(0));
+      //     setIsValidCouponCode(false);
+      //     dispatch(calculatePrice());
+      //   });
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeOutID);
+      // cancel();
+      setIsValidCouponCode(false);
+    };
+  }, [couponCode]);
+
+  const subtotal = 10;
+  const tax = parseInt((subtotal * 0.18).toFixed(2));
+  const shippingCharges = 3;
+  const discount = 2;
+  const total = parseInt((subtotal + tax + shippingCharges - discount).toFixed(2))
+
+  const incrementHandler = (cartItem: CartItem) => {}
+  const decrementHandler = (cartItem: CartItem) => {}
+  const removeHandler = (id: string) => {}
+
   return (
-    <div>Cart</div>
+    <div className="cart">
+      <main>
+        {cartItems.length > 0 ? (
+            cartItems.map((i, idx) => (
+              <CartItemComponent
+                incrementHandler={incrementHandler}
+                decrementHandler={decrementHandler}
+                removeHandler={removeHandler}
+                key={idx}
+                cartItem={i}
+              />
+            ))
+          ) : (
+            <h1>No Items Added</h1>
+          )}
+      </main>
+      <aside>
+        <p>Subtotal: ₹{subtotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount: <em className="red"> - ₹{discount}</em>
+        </p>
+        <p>
+          <b>Total: ₹{total}</b>
+        </p>
+
+        <input
+          type="text"
+          placeholder="Coupon Code"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+        />
+
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              ₹{discount} off using the <code>{couponCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Coupon <VscError />
+            </span>
+          ))}
+
+        {cartItems.length > 0 && <Link to="/shipping">Checkout</Link>}
+      </aside>
+    </div>
   )
 }
 
---
+++ ecommerce-frontend/src/types/types.ts
@@ -0,0 +1,8 @@
+export type CartItem = {
+    productId: string;
+    photo: string;
+    name: string;
+    price: number;
+    quantity: number;
+    stock: number;
+};
