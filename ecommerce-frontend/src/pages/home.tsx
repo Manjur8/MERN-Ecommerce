015dcb0171cdb89473f3802bf57a68b027d8fa9d
Manjur <manjursk209@gmail.com>
2024-09-04 19:52:00 +0530

    added home ui

--- ecommerce-frontend/src/pages/home.tsx
+++ ecommerce-frontend/src/pages/home.tsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom"
+import ProductCard from "../components/ProductCard"
 
 
 const Home = () => {
+  const addToCartHandler = () => {}
   return (
-    <div>Home</div>
+    <div className="home">
+      <section></section>
+
+      <h1>Latest Products
+        <Link to={'/search'} className="findmore">More</Link>
+      </h1>
+
+      <main>
+        <ProductCard productId="productId" name="Macbook" price={59999} stock={5} handler={addToCartHandler} photo="https://m.media-amazon.com/images/I/71h-tsPzk5L._SX679_.jpg"  />
+      </main>
+    </div>
   )
 }
 
