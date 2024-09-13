889872dc41ec8949493c47f02e5af9b3c6460bd0
Manjur <manjursk209@gmail.com>
2024-09-13 13:50:36 +0530

    Login UI

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -7,6 +7,7 @@ const Home = lazy(() => import("./pages/home"))
 const Search = lazy(() => import("./pages/search"))
 const Cart = lazy(() => import("./pages/cart"))
 const Shipping = lazy(() => import("./pages/shipping"))
+const Login = lazy(() => import("./pages/login"))
 
 // ==========Admin Routes===========
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -37,6 +38,9 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+
+          {/* =======Not logged In route====== */}
+            <Route path="/login" element={<Login />} />
 
           {/* ===========Logged in user routes========= */}
           <Route>
