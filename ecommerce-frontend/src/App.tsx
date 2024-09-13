2f280493afc1f30b41e72683b471f99ea5300686
Manjur <manjursk209@gmail.com>
2024-09-13 13:18:48 +0530

    Shipping UI

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Header from "./components/Header"
 const Home = lazy(() => import("./pages/home"))
 const Search = lazy(() => import("./pages/search"))
 const Cart = lazy(() => import("./pages/cart"))
+const Shipping = lazy(() => import("./pages/shipping"))
 
 // ==========Admin Routes===========
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -36,6 +37,11 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+
+          {/* ===========Logged in user routes========= */}
+          <Route>
+            <Route path="/shipping" element={<Shipping />} />
+          </Route>
 
           {/* ========Admin Routes============ */}
           <Route
