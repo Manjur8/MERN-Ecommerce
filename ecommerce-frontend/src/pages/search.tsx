76f60084a036501d788ce24938faf5b2c0d0be42
Manjur <manjursk209@gmail.com>
2024-09-16 09:29:41 +0530

    added search UI

--- ecommerce-frontend/src/pages/search.tsx
+++ ecommerce-frontend/src/pages/search.tsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import { useState } from "react"
+import ProductCard from "../components/ProductCard"
+
 
 const Search = () => {
+  const [search, setSearch] = useState('')
+  const [sort, setSort] = useState('')
+  const [maxPrice, setMaxPrice] = useState(100000)
+  const [category, setCategory] = useState('')
+  const [page, setPage] = useState(1)
+
+  const totalPage=4
+
+  const isPrevPage=page <= 1
+  const isNextPage=page >= totalPage
+
+  const addToCartHandler = () => {}
+
   return (
-    <div>Search</div>
+    <div className="product-search-page">
+      <aside>
+        <h2>Filters</h2>
+        <div>
+          <h4>Sort</h4>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="">None</option>
+            <option value="asc">Price (Low to High)</option>
+            <option value="dsc">Price (High to Low)</option>
+          </select>
+        </div>
+
+        <div>
+          <h4>Max Price: {maxPrice || ""}</h4>
+          <input type="range" min={100} max={100000} value={maxPrice} onChange={(e) => setMaxPrice(Number(e.target.value))} />
+        </div>
+
+        <div>
+          <h4>Category</h4>
+          <select value={category} onChange={(e) => setCategory(e.target.value)}>
+            <option value="">All</option>
+            <option value="camera">Camera</option>
+            <option value="laptop">Laptop</option>
+          </select>
+        </div>
+      </aside>
+
+      <main>
+        <h1>Products</h1>
+        <input type="text" placeholder="Search by name..." value={search} onChange={(e) => setSearch(e.target.value)}/>
+
+        <div className="search-product-list">
+          <ProductCard productId="productId" name="Macbook" price={59999} stock={5} handler={addToCartHandler} photo="https://m.media-amazon.com/images/I/71h-tsPzk5L._SX679_.jpg" />
+        </div>
+
+        <article>
+          <button onClick={() => setPage((prev) => prev-1)} disabled={isPrevPage}>Prev</button>
+          <span>{page} of {totalPage}</span>
+          <button onClick={() => setPage((prev) => prev+1)} disabled={isNextPage}>Next</button>
+        </article>
+      </main>
+    </div>
   )
 }
 
